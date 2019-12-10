bcb906771a7dfd84733ad1a992550b056634a27b
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-12-10 16:47:37 +0100

    Blocco 5 - JSnack 5 Terminato

--- js-jsnacks-blocco-5#5/main.js
+++ js-jsnacks-blocco-5#5/main.js
@@ -4,21 +4,26 @@
 // La funzione ritornerà un nuovo array con i valori che
 // hanno la posizione compresa tra “a” e “b”
 
+var test = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+var final = [];
 
-// ragioniamo per gradi, impostando valori fissi:
-// a è più piccolo di b
-var a = 3;
-var b = 5;
-// b non è più grande della lunghezza dell'array
-var array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-var newArray = [];
-// i limiti entro cui si muove il ciclo for sono il valore di a e b
-for (var i = a; i <= b; i++) {
-    console.log(i);
-    // prendo l'elemento in posizione a
-    var item = array[i];
-    console.log(item);
-    // lo pusho dentro una nuova array
-    newArray.push(item);
-};
-console.log(newArray);
+myChoice(2, 6, test, final);
+
+console.log(final);
+
+
+function myChoice(a, b, array, newArray) {
+    if ((a > b) || (b >= array.length)) {
+        alert('Attenzione, qualcosa nelle impostazioni non va bene');
+        console.log(array.length);
+    } else {
+        // i limiti entro cui si muove il ciclo for sono il valore di a e b
+        for (var i = a; i <= b; i++) {
+            // prendo l'elemento in posizione a
+            var item = array[i];
+            // lo pusho dentro una nuova array
+            newArray.push(item);
+        };
+        return newArray;
+    }
+}
