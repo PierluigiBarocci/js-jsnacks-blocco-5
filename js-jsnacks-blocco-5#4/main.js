3145b43bb1116523a5028f10dfd2a8fe3c5bc232
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-12-10 17:37:02 +0100

    Mi gioco il bonus così: se le lunghezze delle array sono diverse, accorcio la più lunga con .slice(), per renderla della stessa lunghezza dell'altra.

--- js-jsnacks-blocco-5#4/main.js
+++ js-jsnacks-blocco-5#4/main.js
@@ -3,14 +3,25 @@
 // elementi da uno e dall’altro
 // es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
-var letters = ['a', 'b', 'c'];
-var numbers = [1, 2, 3];
+var letters = ['a', 'b', 'c', 'd', 'e', 'f'];
+var numbers = [1, 2, 3, 4, 5, 6, 7];
 var final = [];
 
 console.log(myMixup(letters, numbers, final));
 
 // creo una funzione che ha come argomento 3 array, di cui l'ultima è quella vuota
 function myMixup (first_array, second_array, destination_array) {
+    // BISOGNA MIGLIORARLO: COSA FARE NEL CASO IN CUI GLI ARRAY DI PARTENZA ABBIANO LUNGHEZZA DIFFERENTE?
+    // Li accorcio con .slice e tanti saluti :P
+    if (first_array.length > second_array.length) {
+        console.log('Mi dispiace, la prima array era troppo lunga: ho dovuto accorciarla');
+        var first_array = first_array.slice(0, (second_array.length));
+    };
+    if (first_array.length < second_array.length) {
+        console.log('Mi dispiace, la seconda array era troppo lunga: ho dovuto accorciarla');
+        var second_array = second_array.slice(0, (first_array.length));
+    };
+    console.log(first_array);
     // finchè l'array di destinazione non ha la lunghezza corrispondente alla somma delle lunghezze dei due array di partenza
     while (destination_array.length < ((first_array.length) + (second_array.length))) {
         // inizio a ciclare il primo array
@@ -26,6 +37,3 @@ function myMixup (first_array, second_array, destination_array) {
     // resistuendo un array completamente mischiato
     return destination_array;
 };
-
-// BISOGNA MIGLIORARLO: COSA FARE NEL CASO IN CUI GLI ARRAY DI PARTENZA ABBIANO LUNGHEZZA DIFFERENTE?
-// CI TORNO DOPO AVER FINITO L'ULTIMO SNACK
